245b6e06e69b67f8e3c3f0284ae34ef302cb0888
Meibo <738751286@qq.com>
2024-08-30 09:36:44 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 // https://vitejs.dev/config/
 export default defineConfig({
+  devServer: {
+    port: 8080 // 这里修改为你想要的端口号
+  },
   plugins: [
     uni(),
   ],
